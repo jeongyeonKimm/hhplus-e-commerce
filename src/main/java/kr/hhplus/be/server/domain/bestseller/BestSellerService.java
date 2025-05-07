ed55ccba4325882cc620b42f4bc9fd15456283f8
Jeongyeon Kim <wjddus9132@gmail.com>
2025-05-08 08:47:09 +0900

    refactor: 확인용 로그 제거

--- src/main/java/kr/hhplus/be/server/domain/bestseller/BestSellerService.java
+++ src/main/java/kr/hhplus/be/server/domain/bestseller/BestSellerService.java
@@ -2,7 +2,6 @@ package kr.hhplus.be.server.domain.bestseller;
 
 import kr.hhplus.be.server.domain.bestseller.dto.BestSellerDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
@@ -20,7 +18,6 @@ public class BestSellerService {
 
     @Cacheable(value = "bestSellers", key = "'best'", cacheManager = "redisCacheManager")
     public BestSellerDto getBestSellers() {
-        log.info("🔥🔥🔥 캐시 없이 DB에서 인기 상품 조회 실행");
         List<BestSeller> bestSellers = bestSellerRepository.getBestSellers();
         return BestSellerDto.of(bestSellers);
     }
