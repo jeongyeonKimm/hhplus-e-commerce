a25af8de8f46a6fe5cfdcf5bba86d43047cb77e7
Jeongyeon Kim <wjddus9132@gmail.com>
2025-05-12 14:48:14 +0900

    test: 인기 상품 조회 캐시 테스트

---
+++ src/test/java/kr/hhplus/be/server/application/bestseller/BestSellerCacheTest.java
@@ -0,0 +1,64 @@
+package kr.hhplus.be.server.application.bestseller;
+
+import kr.hhplus.be.server.domain.bestseller.BestSeller;
+import kr.hhplus.be.server.domain.bestseller.BestSellerService;
+import kr.hhplus.be.server.infrastructure.bestseller.BestSellerJpaRepository;
+import kr.hhplus.be.server.support.IntegrationTestSupport;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.instancio.Select.field;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class BestSellerCacheTest extends IntegrationTestSupport {
+
+    @Autowired
+    private BestSellerFacade bestSellerFacade;
+
+    @MockitoBean
+    private BestSellerService bestSellerService;
+
+    @Autowired
+    private BestSellerJpaRepository bestSellerJpaRepository;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @DisplayName("인기 상품 조회를 하면 첫 요청은 DB에 접근하여 데이터를 조회하고, 이후 요청은 캐시에서 데이터를 조회한다.")
+    @Test
+    void getBestSellers_cache() {
+        IntStream.range(1, 101).forEach(i -> {
+            BestSeller bestSeller = Instancio.of(BestSeller.class)
+                    .ignore(field(BestSeller.class, "id"))
+                    .set(field(BestSeller::getTitle), "BestSeller " + i)
+                    .set(field(BestSeller::getDescription), "This is BestSeller " + i)
+                    .set(field(BestSeller::getPrice), (long) (Math.random() * 99000) + 1000)
+                    .set(field(BestSeller::getStock), 100L * i)
+                    .set(field(BestSeller::getSales), 10L * i)
+                    .set(field(BestSeller::getUpdatedAt), LocalDateTime.now().minusDays(1))
+                    .set(field(BestSeller::getCreatedAt), LocalDateTime.now().minusDays(1))
+                    .create();
+
+            bestSellerJpaRepository.saveAndFlush(bestSeller);
+        });
+
+        Object before = redisTemplate.opsForValue().get("bestSellers::best");
+        assertThat(before).isNull();
+
+        bestSellerFacade.getBestSellers();
+
+        Object after = redisTemplate.opsForValue().get("bestSellers::best");
+        assertThat(after).isNotNull();
+
+        verify(bestSellerService, times(1)).getBestSellers();
+    }
+}
